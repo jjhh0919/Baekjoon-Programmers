7055665fb4e871d6c6e86671131e2e490ab426b9
jjhh0919 <79959373+jjhh0919@users.noreply.github.com>
2022-08-30 06:30:19 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 136 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let answer = "";
+
+for (let i = 97; i <= 122; i++) {
+  answer += input.indexOf(String.fromCharCode(i)) + " ";
+}
+console.log(answer);
