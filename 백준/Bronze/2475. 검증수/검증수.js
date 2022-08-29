ff948e0fd627be077b7fa8d2a8a414ee72f3c178
jjhh0919 <79959373+jjhh0919@users.noreply.github.com>
2022-08-30 00:51:38 +0900

    [Bronze V] Title: 검증수, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map((x) => parseInt(x));
+
+let sum = 0;
+
+for (let i = 0; i < 5; i++) {
+  sum += Math.pow(input[i], 2);
+}
+
+console.log(sum % 10);
+
