bca744f225c5ef1733a1380dac35074e6c1deed7
jjhh0919 <79959373+jjhh0919@users.noreply.github.com>
2022-08-30 02:26:25 +0900

    [Bronze V] Title: N 찍기, Time: 240 ms, Memory: 22596 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let answer = "";
+for (let i = 1; i <= parseInt(input); i++) {
+  answer += i + "\n";
+}
+
+console.log(answer);
