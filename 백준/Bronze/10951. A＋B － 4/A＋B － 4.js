5d9ff3bb1a4aab2aa00be55703e26442ca99011d
jjhh0919 <79959373+jjhh0919@users.noreply.github.com>
2022-08-30 19:55:00 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let numArray = [];
+
+for (let i of input) {
+  numArray.push(i.split(" ").map((x) => parseInt(x)));
+}
+
+numArray = numArray.map((x) => x[0] + x[1]);
+
+for (let num of numArray) {
+  console.log(num);
+}
