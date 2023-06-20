34868e39a2a9c639be6c80e58fcaa960107a0909
jjhh0919 <79959373+jjhh0919@users.noreply.github.com>
2023-06-20 18:28:10 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let total = 0;
+
+for(let i = 1; i <= parseInt(input); i++) {
+    total += i;
+}
+
+console.log(total);
